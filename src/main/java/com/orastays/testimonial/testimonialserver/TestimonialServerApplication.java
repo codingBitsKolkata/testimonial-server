2ac2b24361d0bbcd232abd1ac0f0e9c39efa6d24
codingBitsKolkata <45310401+codingBitsKolkata@users.noreply.github.com>
2018-12-06 14:29:54 +0530

    first commit

---
+++ src/main/java/com/orastays/testimonial/testimonialserver/TestimonialServerApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.testimonial.testimonialserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestimonialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestimonialServerApplication.class, args);
+	}
+}
