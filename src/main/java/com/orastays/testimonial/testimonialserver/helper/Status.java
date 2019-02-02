ac537a261e78dddf24d5e8193d8fcc8f0c22486c
Avirup <avirup.pal@gmail.com>
2019-02-02 22:02:48 +0530

    package names changes

--- src/main/java/com/orastays/testimonial/testimonialserver/helper/Status.java
+++
@@ -1,6 +0,0 @@
-package com.orastays.testimonial.testimonialserver.helper;
-
-public enum Status {
-
-	ZERO, ACTIVE, INACTIVE, DELETE
-}
--- src/main/java/com/orastays/testimonial/testimonialserver/service/DatabaseService.java
+++
@@ -1,7 +0,0 @@
-package com.orastays.testimonial.testimonialserver.service;
-
-public interface DatabaseService {
-	
-	public abstract String databaseOperation(String query) throws Exception ;
-	
-}
